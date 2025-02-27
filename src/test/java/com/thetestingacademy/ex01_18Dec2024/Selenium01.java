94ec93ae1ef86e6607c6a646617a454bb42ebbc3
Preeti <preetisilla18@gmail.com>
2025-02-27 13:24:03 +0530

    Selenium project

---
+++ src/test/java/com/thetestingacademy/ex01_18Dec2024/Selenium01.java
@@ -0,0 +1,25 @@
+package com.thetestingacademy.ex01_18Dec2024;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Selenium01 {
+    @Test
+    public void test_Selenium(){
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://app.vwo.com");
+        driver.quit();
+
+        EdgeOptions edgeOptions = new EdgeOptions();
+        edgeOptions.addArguments("--window-size=1920,1080");
+        edgeOptions.addArguments("--incognito");
+
+        EdgeDriver edriver = new EdgeDriver(edgeOptions);
+        edriver.get("https://app.vwo.com");
+        edriver.quit();
+
+    }
+}
