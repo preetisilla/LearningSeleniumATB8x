134740f589ac19448e78e1408a397ad35ab597e4
Preeti <preetisilla18@gmail.com>
2025-03-04 07:11:11 +0530

    Selenium alert

--- src/test/java/com/thetestingacademy/SeleniumWaitandBox/Selenium18_JSAlerts.java
+++ src/test/java/com/thetestingacademy/SeleniumWaitandBox/Selenium18_JSAlerts.java
@@ -1,10 +1,16 @@
 package com.thetestingacademy.SeleniumWaitandBox;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class Selenium18_JSAlerts {
     @Test
@@ -16,11 +22,28 @@ public class Selenium18_JSAlerts {
         WebElement elementConfirm = driver.findElement(By.xpath("//button[contains(text(),'Confirm')]"));
         WebElement elementPrompt = driver.findElement(By.xpath("//button[contains(text(),'Prompt')]"));
 
+        alertElement.click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+        wait.until(ExpectedConditions.alertIsPresent());
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
 
+        WebElement result = driver.findElement(By.id("result"));
+        Assert.assertEquals(result.getText(),"You successfully clicked an alert");
 
+        elementConfirm.click();
+        Alert alert1 = driver.switchTo().alert();
+        alert1.accept();
+       // alert1.dismiss(); to cancel
 
+        Assert.assertEquals(result.getText(),"You clicked: Ok");
 
+        elementPrompt.click();
+        Alert alert2 = driver.switchTo().alert();
+        alert2.sendKeys("Preeti");
+        alert2.accept();
 
+        Assert.assertEquals(result.getText(),"You entered: Preeti");
 
     }
 }
